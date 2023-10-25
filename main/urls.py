cda092db018ffdf8c3399982dde98cb15996b60e
bryanjeshua <bryanjmt04@gmail.com>
2023-10-25 15:48:00 +0700

    initialized

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_landing_page
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('', show_landing_page, name='show_landing_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def show_main(request):
+def show_landing_page(request):
     context = {
         'name': 'Pak Bepe',
         'class': 'PBP A'
     }
-
-    return render(request, "main.html", context)
+    return render(request, "landingpage.html", context)
