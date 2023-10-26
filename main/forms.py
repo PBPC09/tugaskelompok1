6bdd13c0744e66d5c9f90000ca4882404d71afca
clarista <clarista05@gmail.com>
2023-10-26 19:26:48 +0700

    commit2

---
+++ main/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+class SignUpForm(UserCreationForm):
+    ROLE_CHOICES = [('S', 'Seller'), ('B', 'Buyer')]
+    role = forms.ChoiceField(
+        label="Choose Your User Type",
+        required=True,
+        choices=ROLE_CHOICES
+    )
+    email = forms.EmailField(max_length=200)
+
+    class Meta:
+        model = User
+        fields = ['role', 'first_name', 'last_name', 'username', 'email', 'password1']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+        
+        profile = Profile.objects.create(
+            user=user,
+            role=self.cleaned_data["role"],
+            first_name=self.cleaned_data["first_name"], 
+            last_name=self.cleaned_data["last_name"],  
+            email=self.cleaned_data["email"],
+        )
+        return user
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AddToWishlistForm
+from .models import Book, Wishlist
+from django.http import JsonResponse
 
-# Create your views here.
+def bookprofile(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == "POST":
+        form = AddToWishlistForm(request.POST)
+        if form.is_valid():
+            preference = form.cleaned_data['preference']
+            Wishlist.objects.create(user=request.user, book=book, preference=preference)
+            return redirect('wishlist_page')
+    else:
+        form = AddToWishlistForm()
+    return render(request, 'bookprofile.html', {'form': form})
+
+# def bookprofile(request):
+#     form = AddToWishlistForm()
+#     return render(request, 'bookprofile.html', {'form': form})
+
+def wishlistpage(request):
+    wishlist_items = Wishlist.objects.filter(user=request.user)
+    return render(request, 'wishlist.html', {'wishlist_items': wishlist_items})
+
+
+def addtowishlist(request, book_id):
+    if request.method == "POST":
+        form = AddToWishlistForm(request.POST)
+        if form.is_valid():
+            preference = form.cleaned_data['preference']
+            Wishlist.objects.create(user=request.user, book_id=book_id, preference=preference)
+            return JsonResponse({'status': 'success'})
+        else:
+            return JsonResponse({'status': 'error'})
+
