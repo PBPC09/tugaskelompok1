f1a5b3e36d7ae37f843f4c097417fb0257d06098
bryanjeshua <bryanjmt04@gmail.com>
2023-10-26 19:04:37 +0700

    first try form

--- bookforum/urls.py
+++ bookforum/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from bookforum.views import *
 
 app_name = 'bookforum'
 
 urlpatterns = [
+    path('', show_forum, name='show_forum' ),
+    path('create_question/', create_question, name='create_question'),
+    path('create_comments/<int:pk>', create_comments, name="create_comments"),
+    path('delete_question/<int:id>', delete_question, name="delete_question"),
+    path('delete_comments/<int:id>', delete_comments, name="delete_comments"),
+    path('forum/json/', show_forum_json, name="show_forum_json"),
+    path('comments/json/', show_comments_json, name="show_comments_json"),
 ]
--- bookforum/views.py
+++ bookforum/views.py
@@ -1,3 +1,92 @@
-from django.shortcuts import render
-
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from bookforum.models import ForumHead, ForumComment
+from django.shortcuts import render, redirect, get_object_or_404
+import datetime
+from datetime import datetime
+from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
+# @login_required(login_url='/login/')
+def show_forum(request):
+    questions = ForumHead.objects.all()
+    comments = ForumComment.objects.all()
+    context = {
+        'name':request.user.username,
+        'questions' : questions,
+        'comments': comments,
+    }
+    return render(request, "forum.html", context)
+
+# @login_required(login_url='/login/')
+@csrf_exempt
+def create_question(request):
+    if request.method == 'POST':
+        user = request.user
+        book = request.POST.get('book')
+        question = request.POST.get('question')
+        date = datetime.now()
+        title = request.POST.get('title')
+        new_question = ForumHead(user = user, book = book, date = date , title = title, question = question)
+        result = {
+            'pk' : new_question.pk,
+            'fields' : {
+                'username' : new_question.user.username,
+                'title' : new_question.title,
+                'question' : new_question.question,
+                'date' : new_question.date
+            }
+        }
+        new_question.save()
+        return JsonResponse(result)
+    return HttpResponseNotFound()
+
+# @login_required(login_url='/login/')
+@csrf_exempt
+def create_comments(request, pk):
+    forum_head = ForumHead.objects.filter(pk = pk)
+    if request.method == 'POST':
+        user = request.user
+        comment_to = forum_head 
+        date = datetime.date.today()
+        answer = request.POST.get('answer')
+
+        new_comment = ForumComment(user = user, comment_to = comment_to, date = date, answer = answer)
+        result = {
+            'pk' : new_comment.pk,
+            'fields' : {
+                'username' : new_comment.user.username,
+                'comment' : new_comment.answer,
+                'date' : new_comment.date
+            }
+        }
+        return JsonResponse(result)
+    return HttpResponseNotFound()
+
+# @login_required(login_url='/login/')
+@csrf_exempt
+def delete_question(request, id):
+    if request.method == 'DELETE':
+        ForumHead.objects.get(pk = id).delete()
+        return HttpResponse(b"DELETED", status=201)
+    return HttpResponseNotFound()
+
+# @login_required(login_url='/login/')
+@csrf_exempt
+def delete_comments(request, id):
+    if request.method == 'DELETE':
+        ForumComment.objects.get(pk = id).delete()
+        return HttpResponse(b"DELETED", status=201)
+    return HttpResponseNotFound()
+
+def show_forum_json(request):
+    data = ForumHead.objects.all()
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
+
+def show_comments_json(request):
+    data = ForumComment.objects.all()
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
