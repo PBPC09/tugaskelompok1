0f7e52833cebebf4e7d329f36252b5f95a70f9a6
bryanjeshua <bryanjmt04@gmail.com>
2023-10-26 20:48:34 +0700

    third merge

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,25 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from registerbook.models import Book
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey('registerbook.Book', on_delete=models.CASCADE)  # Gantikan 'main_app' dengan nama aplikasi Anda
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)  # Gantikan 'main_app' dengan nama aplikasi Anda
     preference = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
 
     def __str__(self):
         return f"{self.user.username}'s Wishlist - {self.book.title}"
-    
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    genre = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    rating = models.IntegerField()
-    voters = models.IntegerField()
-    price = models.IntegerField()
-    currency = models.CharField(max_length=20)
-    decscription = models.TextField()
-    publisher = models.CharField(max_length=300)
-    page_count = models.IntegerField()
-
-    def __str__(self):
-        return self.title
