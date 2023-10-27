61efd8bc9ac8436fd0888e131ca1b6807b923b2b
lovaire <rifsrahmt@gmail.com>
2023-10-27 12:26:45 +0700

    model.py

--- buybooks/models.py
+++ buybooks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Buybooks(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    voters = models.IntegerField()
+    price = models.IntegerField()
+    currency = models.CharField(max_length=20)
+    decscription = models.TextField()
+    publisher = models.CharField(max_length=300)
+    page_count = models.IntegerField()
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.title
