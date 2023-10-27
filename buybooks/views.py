6b9e29d7ba1e585490c3cbef0705d377eb325062
lovaire <rifsrahmt@gmail.com>
2023-10-27 19:31:44 +0700

    adding func show book

--- buybooks/views.py
+++ buybooks/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from registerbook.models import Book
 
 # Create your views here.
 def show_test(request):
     context = {}
     return render(request, 'buybooks.html', context)
+
+def show_book(request):
+    book = Book.objects.all()
+
+    context = {
+        'book' : book
+    }
+    return render(request, 'buybooks.html')
