59fbda0f5f476bbbdf0a0ba52f6bed6bca4aee28
lovaire <rifsrahmt@gmail.com>
2023-10-27 13:05:15 +0700

    models.py

--- buybooks/models.py
+++ buybooks/models.py
@@ -1,18 +1,9 @@
 from django.db import models
+from registerbook.models import Book
 
-# Create your models here.
-class Buybooks(models.Model):
-    title = models.CharField(max_length=200)
-    genre = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    rating = models.IntegerField()
-    voters = models.IntegerField()
-    price = models.IntegerField()
-    currency = models.CharField(max_length=20)
-    decscription = models.TextField()
-    publisher = models.CharField(max_length=300)
-    page_count = models.IntegerField()
+class Purchase(models.Model):
     amount = models.IntegerField()
+    author = models.ForeignKey(Book, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- registerbook/models.py
+++ registerbook/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-from registerbook.models import Book
 
-class Purchase(models.Model):
-    amount = models.IntegerField()
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    voters = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    currency = models.CharField(max_length=20)
+    description = models.TextField()
+    publisher = models.CharField(max_length=300)
+    page_count = models.IntegerField()
+    genres = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
