9d4d3161ad9b6b8550b4e4c483efd34117e1cbda
bryanjeshua <bryanjmt04@gmail.com>
2023-10-28 12:31:41 +0700

    buybooks

--- buybooks/models.py
+++ buybooks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from registerbook.models import Book
 
 # Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    is_ordered = models.BooleanField(default=False)
+
+    def subtotal(self):
+        return self.book.price * self.quantity
+    
+    def __str__(self):
+        return f"{self.user.username}'s Cart - {self.book.title}"
--- buybooks/views.py
+++ buybooks/views.py
@@ -1,6 +1,38 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from .models import Book, CartItem
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def show_test(request):
-    context = {}
+@login_required
+def show_buybooks(request):
+    books = Book.objects.all()
+    context = {
+        'book': books,
+    }
     return render(request, 'buybooks.html', context)
+
+def add_cart_ajax(request):
+    if request.method == 'POST':
+        # book = request.POST.get("book")
+        # price = request.POST.get("price")
+        return
+
+def show_cart(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    context = {
+        'cart_items': cart_items,
+    }
+    return render(request, 'buybooks.html', context)
+
+def get_cart_json(request):
+    items = CartItem.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', items))
+
+def delete_cart_ajax(request, id):
+    data = json.loads(request.body.decode("utf-8"))
+    item = CartItem.objects.get(pk=data["id"])
+    item.delete()
+    return HttpResponse("DELETED",status=200)
