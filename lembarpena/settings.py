699010e6064d316b2d60467ae17a58dd61901f35
bryanjeshua <bryanjmt04@gmail.com>
2023-10-29 22:43:30 +0700

    secure cross origin

--- lembarpena/settings.py
+++ lembarpena/settings.py
@@ -137,3 +137,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
