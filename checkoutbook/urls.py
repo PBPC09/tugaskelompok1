12961ffc6930051e63a9c58f0c1342efc5800c2e
pershyfon <sabrina.aviana@ui.ac.id>
2023-10-29 08:28:40 +0700

    buybooks, cart, dan checkout

--- checkoutbook/urls.py
+++ checkoutbook/urls.py
@@ -6,4 +6,10 @@ app_name = 'checkoutbook'
 urlpatterns = [
     path('', show_checkout, name='checkout'),
     path('checkout_ajax/', checkout_ajax, name='checkout_ajax'),
+    path('myorder/', show_myorder, name="myorder"),
+    path('get_order_json/', get_order_json, name="get_order_json"),
+    path('get_order_json_cc/', get_order_json_cc, name="get_order_json_cc"),
+    path('get_order_json_dc/', get_order_json_dc, name="get_order_json_dc"),
+    path('get_order_json_tf/', get_order_json_tf, name="get_order_json_tf"),
+    path('get_order_json_ew/', get_order_json_ew, name="get_order_json_ew"),
 ]
--- checkoutbook/views.py
+++ checkoutbook/views.py
@@ -1,7 +1,8 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse
 from django.urls import reverse
-from django.http import JsonResponse
+from django.core import serializers
 from .models import CartItem, Checkout
 from .forms import CheckoutForm
 from django.contrib.auth.decorators import login_required
@@ -24,8 +25,12 @@ def checkout_ajax(request):
         items = CartItem.objects.filter(user=request.user, is_ordered=True)
         alamat = request.POST.get("alamat")
         metode_pembayaran = request.POST.get("metode_pembayaran")
+        total_price = sum(item.subtotal() for item in items)
 
-        new_item = Checkout(user=user, items=items, alamat=alamat, metode_pembayaran=metode_pembayaran)
+        new_item = Checkout(user=user, alamat=alamat, metode_pembayaran=metode_pembayaran, total_price=total_price)
+        new_item.save()
+
+        new_item.items.set(items)
         new_item.save()
 
         for item in items:
@@ -34,3 +39,31 @@ def checkout_ajax(request):
         return HttpResponseRedirect(reverse('buybooks:show_cart'))
 
     return HttpResponseNotFound()
+
+@login_required(login_url='/login')
+def show_myorder(request):
+    orders = Checkout.objects.all()
+    context = {
+        'orders': orders
+    }
+    return render(request, 'myorder.html', context)
+
+def get_order_json(request):
+    orders = Checkout.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', orders))
+
+def get_order_json_cc(request):
+    orders = Checkout.objects.filter(user=request.user, metode_pembayaran="kartu_kredit")
+    return HttpResponse(serializers.serialize('json', orders), content_type="application/json")
+
+def get_order_json_dc(request):
+    orders = Checkout.objects.filter(user=request.user, metode_pembayaran="kartu_debit")
+    return HttpResponse(serializers.serialize('json', orders), content_type="application/json")
+
+def get_order_json_tf(request):
+    orders = Checkout.objects.filter(user=request.user, metode_pembayaran="transfer_bank")
+    return HttpResponse(serializers.serialize('json', orders), content_type="application/json")
+
+def get_order_json_ew(request):
+    orders = Checkout.objects.filter(user=request.user, metode_pembayaran="e_wallet")
+    return HttpResponse(serializers.serialize('json', orders), content_type="application/json")
