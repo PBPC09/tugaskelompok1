9a3119aa3324f02a22e7110f88817ace8118dd95
bryanjeshua <bryanjmt04@gmail.com>
2023-10-29 22:02:24 +0700

    set STATIC only one path

--- lembarpena/settings.py
+++ lembarpena/settings.py
@@ -95,7 +95,6 @@ if PRODUCTION:
     }
     DATABASES["default"]["ATOMIC_REQUESTS"] = True
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
@@ -132,8 +131,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
