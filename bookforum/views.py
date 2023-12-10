ae59aca60785a454cae3b0113129ce26d75161f6
bryanjeshua <bryanjmt04@gmail.com>
2023-12-10 22:26:31 +0700

    edit: udah bisa CRUD semua

--- bookforum/views.py
+++ bookforum/views.py
@@ -62,6 +62,35 @@ def create_question(request):
 
 @login_required(login_url='/login/')
 @csrf_exempt
+def create_question_flutter(request):
+    if request.method == 'POST':
+        user = request.user
+        # user = USER_BARU
+        data = json.loads(request.body)
+        book_id = data['book_id']
+        book = Book.objects.get(pk=book_id)
+        question = data['question']
+        date = datetime.now()
+        title = data['title']
+        new_question = ForumHead(user = user, book = book, date = date , title = title, question = question)
+        # result = {
+        #     'pk' : new_question.pk,
+        #     'fields' : {
+        #         'username' : new_question.user.username,
+        #         'title' : new_question.title,
+        #         'question' : new_question.question,
+        #         'date' : new_question.date,
+        #         'book' : new_question.book.title,
+        #         'book_id' : new_question.book,
+        #     }
+        # }
+        new_question.save()
+        return JsonResponse({"status": "success"}, status=200)
+    return JsonResponse({"status": "error"}, status=401)
+
+
+@login_required(login_url='/login/')
+@csrf_exempt
 def create_comments(request, pk):
     forum_head = ForumHead.objects.get(pk = pk)
     if request.method == 'POST':
@@ -84,6 +113,52 @@ def create_comments(request, pk):
 
 @login_required(login_url='/login/')
 @csrf_exempt
+def create_comments_flutter(request, pk):
+    if request.method == 'POST':
+        forum_head = ForumHead.objects.get(pk = pk)
+        data = json.loads(request.body)
+        user = request.user
+        # user = USER_BARU
+        date = datetime.now()
+        # answer = request.POST.get('answer')
+        new_comment = ForumComment(user = user, comment_to = forum_head, date = date, answer = data["answer"])
+        result = {
+            'pk' : new_comment.pk,
+            'fields' : {
+                'username' : new_comment.user.username,
+                'comment' : new_comment.answer,
+                'date' : new_comment.date
+            }
+        }
+        new_comment.save()
+        return JsonResponse({"status": "success"}, status=200)
+
+        # return JsonResponse(result)
+    return JsonResponse({"status": "error"}, status=401)
+# @login_required(login_url='/login/')
+# @csrf_exempt
+# def create_comments_flutter(request, id):
+#     if request.method == 'POST':
+#         forum_head = ForumHead.objects.get(pk = pk)
+
+#         data = json.loads(request.body)
+
+#         new_product = Product.objects.create(
+#             user = request.user,
+#             name = data["name"],
+#             price = int(data["price"]),
+#             description = data["description"],
+#             categories = data["categories"]
+#         )
+
+#         new_product.save()
+
+#         return JsonResponse({"status": "success"}, status=200)
+#     else:
+#         return JsonResponse({"status": "error"}, status=401)
+
+@login_required(login_url='/login/')
+@csrf_exempt
 def delete_question(request, username, id):
     if request.method == 'GET' and request.user.username == username:
         ForumHead.objects.get(pk = id).delete()
@@ -92,11 +167,27 @@ def delete_question(request, username, id):
 
 @login_required(login_url='/login/')
 @csrf_exempt
+def delete_question_flutter(request, username, id):
+    if request.method == 'POST' and request.user.username == username:
+        ForumHead.objects.get(pk = id).delete()
+        return JsonResponse({'status': 'success'}, status=200)
+    return JsonResponse({'status': 'failed'}, status=300)
+
+@login_required(login_url='/login/')
+@csrf_exempt
 def delete_comments(request, username, id):
     if request.method == 'GET' and request.user.username == username:
         ForumComment.objects.get(pk = id).delete()
         return HttpResponse(b"DELETED", status=201)
     return HttpResponseNotFound()
+
+@login_required(login_url='/login/')
+@csrf_exempt
+def delete_comments_flutter(request, username, id):
+    if request.method == 'POST' and request.user.username == username :
+        ForumComment.objects.get(pk = id).delete()
+        return JsonResponse({'status': 'success'}, status=200)
+    return JsonResponse({'status': 'failed'}, status=300)
 
 def show_forum_json_2(request):
     data = ForumHead.objects.all()
