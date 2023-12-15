5c6ca9255ef868993a1a0542b1001ebaee7f9b45
hilaldfzn <hilalfauzan9@gmail.com>
2023-12-15 11:00:51 +0700

    regist model notification ke admin

--- registerbook/admin.py
+++ registerbook/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Book
-from registerbook.models import Book
+from registerbook.models import Book, Notification
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(Notification)
