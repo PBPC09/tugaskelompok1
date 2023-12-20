bd246968c7881c5be1c40fa64d9c2b9e2c4423a1
bryanjeshua <bryanjmt04@gmail.com>
2023-12-20 17:46:39 +0700

    edit: push to main

--- checkoutbook/urls.py
+++ checkoutbook/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('myorder/', show_myorder, name="myorder"),
     path('get_order_json/', get_order_json, name="get_order_json"),
     path('get_order_json_all/', get_order_json_all, name="get_order_json_all"),
-
     path('get_order_json_cc/', get_order_json_cc, name="get_order_json_cc"),
     path('get_order_json_dc/', get_order_json_dc, name="get_order_json_dc"),
     path('get_order_json_tf/', get_order_json_tf, name="get_order_json_tf"),
